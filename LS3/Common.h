636fa06c96b07112577517bedf690515b037372d
Maksym <gurinmaxim11@gmail.com>
2021-12-14 17:15:56 +0200

    ls3

---
+++ LS3/Common.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <iomanip>
+#include <utility>
+#include <fstream>
+#include <sstream>
+#include <optional>
+#include <algorithm>
+#include <tuple>
+#include <chrono>
+#include <list>
+#include <initializer_list>
+
---
+++ LS3/main.cpp
@@ -0,0 +1,28 @@
+﻿//#include "Header.h"
+#include "Common.h"
+
+template<class C, class T>
+void insert_sorted(C& container, T value)
+{
+	auto it{ lower_bound(container.begin(), container.end(), value) };
+	container.insert(it, value);
+}
+
+
+int main()
+{
+	std::vector<int> cont{ 1,2,3,4,5,6,7 };
+	insert_sorted(cont, 3);
+	insert_sorted(cont, 8);
+
+	for (const auto& el : cont)
+	{
+		std::cout << el << " ";
+	}
+	std::cout << std::endl;
+
+
+
+	return 0;
+}
+
